673090cea41fbe0027b7151b698e4f1271b870d0
akshaynkotkar <akshaynkotkar@gmail.com>
2024-11-15 20:33:18 +0530

    added use of @RequestParam method

--- src/main/java/com/example/akshaykotkar/springbootwebtutorial/controllers/EmployeeController.java
+++ src/main/java/com/example/akshaykotkar/springbootwebtutorial/controllers/EmployeeController.java
@@ -1,10 +1,7 @@
 package com.example.akshaykotkar.springbootwebtutorial.controllers;
 
 import com.example.akshaykotkar.springbootwebtutorial.dto.EmployeeDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Struct;
 import java.time.LocalDate;
@@ -22,8 +19,9 @@ public class EmployeeController {
         return new EmployeeDTO(employeeId,"Akshay","ak@gmail.com",21, LocalDate.of(2024,11,15), true);
     }
     /*
-    {
+
    url: http://localhost:8080/employees/1
+{
   "id": 1,
   "name": "Akshay",
   "email": "ak@gmail.com",
@@ -31,5 +29,19 @@ public class EmployeeController {
   "dateOfJoining": "2024-11-15",
   "active": true
 }
+     */
+
+    @GetMapping(path="/employees") // (path="/employees") we can use it for parent so no need to declare it every time
+    //Using @RequestMapping(path="/employees") need to use for it.
+    public  String getAllEmployees(@RequestParam(required = false) Integer age,@RequestParam(required = false) String sortBy)
+    {
+        return  "Hi age "+age+" "+sortBy;
+    }
+    /*
+    http://localhost:8080/employees?age=21
+    o/p= Hi age 21 null
+    http://localhost:8080/employees?age=21&sortBy=name
+    O/p=Hi age 21 name
+
      */
 }
